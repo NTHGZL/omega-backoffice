7ccc5d5b67d84b2d651feafe2301e023d3e0abf9
NTHGZL <nathan_gonzalez@hotmail.fr>
2023-03-03 15:51:16 +0100

    fix

--- scripts/script.sh
+++ scripts/script.sh
@@ -14,7 +14,7 @@ sed -i "s/\$port/$2/g" instances/$1/docker-compose.yml
 
 
 # Copy the nginx config file to the nginx folder
-cp scripts/nginx.conf /etc/nginx/sites-available/$1.conf
+cp scripts/nginx.example.conf /etc/nginx/sites-available/$1.conf
 # In the nginx file, replace $society with the args
 sed -i "s/\$society/$1/g" /etc/nginx/sites-available/$1.conf
 # In the nginx file, replace $port by 8123 + the number of container running
